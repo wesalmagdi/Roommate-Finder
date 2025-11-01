a3ee06aa581ae893c0e41c7f9d6fd34f8f5b80b5
Ramamohamed2005 <rama.mohamed2005@gmail.com>
2025-11-01 12:52:00 +0200

    Added Navbar component with bootstrap styling

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+function Navbar() {
+  return(
+    <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <a className="navbar-brand" href="#">Roommate Finder</a>
+          <button className="navbar-toggler" 
+          type="button" 
+          data-toggle="collapse" 
+          data-target="#navbarNav" 
+          aria-controls="navbarNav" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/register">
+                Register
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                Login
+              </a>
+
+            </li>
+            
+           
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar;
+ 
