7b52ed8d56b060008b6250792f44f918a388ad71
Ramamohamed2005 <rama.mohamed2005@gmail.com>
2025-11-17 23:36:05 +0200

    Save local changes before merging login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar';
-
-
-
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
+      
+      <BrowserRouter>
       <Navbar />
+        <Routes>
+          
+          <Route path="/register" element={<RegisterScreen />} />
+          <Route path="/login" element={<LoginScreen />} />
+        </Routes>
+      </BrowserRouter>
+
+
+     
+      
+
+
+     
     </div>
   );
 }
-   
-
 
 export default App;
-
-
