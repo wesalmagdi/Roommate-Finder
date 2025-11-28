57a5f67f737f2bfff08cb72550115104800c9982
Ramamohamed2005 <rama.mohamed2005@gmail.com>
2025-11-28 23:15:01 +0200

    Added Aboutus section in NavBar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -20,34 +20,58 @@ function Navbar() {
         isScrolled ? 'navbar-scrolled' : 'navbar-default'
       }`}
     >
-      <Link className="navbar-brand" to="/">
-        Roommate Finder
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <div className="navbar-container">
+        {/* Left Section - Brand */}
+        <div className="nav-left">
+          <Link className="navbar-brand" to="/">
+            Roommate Finder
+          </Link>
+        </div>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item active">
-            <Link className="nav-link" to="/register">
-              Register
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/login">
-              Login
-            </Link>
-          </li>
-        </ul>
+        {/* Center Section - About Us */}
+        <div className="nav-center">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About Us
+              </Link>
+            </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/help">
+                  Help
+                </Link>
+              </li>
+          </ul>
+        </div>
+
+        {/* Right Section - Auth Links */}
+        <div className="nav-right">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Mobile Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
       </div>
     </nav>
   );
